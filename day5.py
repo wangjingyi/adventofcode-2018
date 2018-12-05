20b561818b1bdff9bd186cd35886341288e187c1
Wang, Jingyi <jwang@one.verizon.com>
2018-12-05 11:17:03 -0600

    day5 solution

---
+++ day5.py
@@ -0,0 +1,42 @@
+import string
+
+def solution1():
+    return react(data())
+
+def solution2():
+    s = data()
+    return min(react(s, bad_unit=ch) for ch in string.ascii_lowercase)
+
+def react(s, bad_unit = None):  
+    if not s:
+        return 0
+    
+    i = 0
+    while bad_unit is not None and s[i].lower() == bad_unit:
+        i += 1
+        
+    leftover = [s[i]]
+    for ch in s[i + 1:]:
+        if ch.lower() == bad_unit:
+            continue
+        
+        if not leftover:
+            leftover.append(ch)
+        else:
+            last = leftover.pop()
+            if ch != last and ch.lower() == last.lower():
+                continue
+            else:
+                leftover.extend((last, ch))
+            
+    return len(leftover)
+
+
+def data():
+    with open('input.txt') as f:
+        return f.read().replace('\n', '')
+    
+
+
+
+        
